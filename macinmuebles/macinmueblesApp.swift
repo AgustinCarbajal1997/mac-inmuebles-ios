f77d09aea78cc38ea48b9674129f552baba04945
Agustin Carbajal <magustincarbajal97@gmail.com>
2024-07-21 17:23:02 -0300

    Initial Commit

---
+++ macinmuebles/macinmueblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  macinmueblesApp.swift
+//  macinmuebles
+//
+//  Created by Agustin Carbajal on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct macinmueblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
